231b5aaa22b5fd2d19fba8d439a1cdaeacd90b83
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2024-04-23 16:06:57 +0700

    Create todo

--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -3,7 +3,19 @@
 namespace App\Services\Impl;
 
 use App\Services\TodolistService;
+use Illuminate\Support\Facades\Session;
 
 class TodolistServiceImpl implements TodolistService
 {
+    public function saveTodo(string $id, string $todo): void
+    {
+        if (!Session::has('todolists')) {
+            Session::put('todolists', []);
+        }
+
+        Session::push('todolists', [
+            'id' => $id,
+            'todo' => $todo
+        ]);
+    }
 }
--- tests/Feature/TodolistServiceTest.php
+++ tests/Feature/TodolistServiceTest.php
@@ -21,4 +21,23 @@ class TodolistServiceTest extends TestCase
     {
         self::assertNotNull($this->todolistService);
     }
+
+    public function testSaveTodo()
+    {
+        $this->todolistService->saveTodo('1', 'Belajar Laravel');
+        $this->todolistService->saveTodo('2', 'Belajar PHP');
+        $this->todolistService->saveTodo('3', 'Belajar Java');
+
+        $todolists = session('todolists');
+        self::assertCount(3, $todolists);
+
+        self::assertEquals('1', $todolists[0]['id']);
+        self::assertEquals('Belajar Laravel', $todolists[0]['todo']);
+
+        self::assertEquals('2', $todolists[1]['id']);
+        self::assertEquals('Belajar PHP', $todolists[1]['todo']);
+
+        self::assertEquals('3', $todolists[2]['id']);
+        self::assertEquals('Belajar Java', $todolists[2]['todo']);
+    }
 }
