14ceaa6d2976584de818e0c732133cbbafc80567
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2024-04-23 15:47:15 +0700

    Make: home controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,13 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/template', function () {
-    return view('template');
-});
+Route::get('/', [HomeController::class, 'home']);
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/login', 'login')->middleware('guest');
