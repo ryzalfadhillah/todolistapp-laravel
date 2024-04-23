ee1d87b83eb2bb9cdff2f2affe4d23b892d0c85b
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2024-04-23 16:29:33 +0700

    create todolist controller

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    public function todolist(Request $request)
+    {
+    }
+
+    public function addTodo(Request $request)
+    {
+    }
+
+    public function deleteTodo(Request $request, string $id)
+    {
+    }
+}
