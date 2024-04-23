87feecb3849e4a3ab7c78a28b603b5b8a36335b1
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2024-04-23 10:24:00 +0700

    Create user service

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\UserServiceProvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -176,7 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\UserServiceProvider::class,
     ],
 
     /*
